dedb6ec6e4c04e29f8548a1fc6fbc75f6ac0c75c
Eliza Hynek <eliza.hynek@gmail.com>
2023-08-18 21:39:03 +0200

    add orders endpoints

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
-import { OrderService } from './order.service';
+import { OrdersService } from './orders.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrderService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,9 +7,11 @@ export class ProductsService {
   public getAll(): Product[] {
     return db.products;
   }
+	
   public getById(id: Product['id']): Product | null {
 		return db.products.find((p) => p.id === id);
 	}
+
 	public deleteById(id: Product['id']): void {
 		db.products = db.products.filter((p) => p.id !== id);
 	}

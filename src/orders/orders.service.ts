bef05879c703e1036cd68ab09bacbf4a86b060eb
Eliza Hynek <eliza.hynek@gmail.com>
2023-08-20 22:37:25 +0200

    add orders endpoints

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Order, db } from 'src/db';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class OrdersService {
@@ -11,4 +12,23 @@ export class OrdersService {
 	public getById(id: Order['id']): Order | null {
 		return db.orders.find((o) => o.id === id);
 	}
+
+  public deleteById(id: Order['id']): void {
+		db.orders = db.orders.filter((p) => p.id !== id);
+	}
+
+	public create(orderData: Omit<Order, 'id'>): Order {
+		const newOrder = { ...orderData, id: uuidv4() };
+		db.orders.push(newOrder);
+		return newOrder;
+	}
+
+	public updateById(id: Order['id'], orderData: Omit<Order, 'id'>): void {
+    db.orders = db.orders.map((o) => {
+      if (o.id === id) {
+        return { ...o, ...orderData };
+      }
+      return o;
+    });
+  }
 }

0cfd3f9287c227b079e413fef664dbd9fd7cd7c7
Eliza Hynek <eliza.hynek@gmail.com>
2023-08-31 17:52:00 +0200

    add relations to order endpoints

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -8,12 +8,13 @@ export class OrdersService {
   constructor(private prismaService: PrismaService) {}
   
   public getAll(): Promise<Order[]> {
-    return this.prismaService.order.findMany();
+    return this.prismaService.order.findMany({ include: { product: true } });
   }
 
 	public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
       where: { id },
+      include: { product: true },
     });
   }
 
@@ -41,9 +42,15 @@ export class OrdersService {
     id: Order['id'],
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
+    const { productId, ...otherData } = orderData;
     return this.prismaService.order.update({
       where: { id },
-      data: orderData,
+      data: {
+        ...otherData,
+        product: {
+          connect: { id: productId },
+        },
+      },
     });
   }
 }
